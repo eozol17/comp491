0b880fb977709d66129b038c49171196b211838e
eozol17 <eozol17@ku.edu.tr>
2022-03-28 09:47:02 +0300

    Database Connection Done
    Established connection read, write to database.

--- TermProject-Comp491/CompTermProject/CompTermProject/AppDelegate.swift
+++ TermProject-Comp491/CompTermProject/CompTermProject/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import FirebaseFirestore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
-        let db = Firestore.firestore()
+
         return true
     }
 
