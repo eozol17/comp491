c169aa35f2dd4886f4fb5fc3a8ff629138548612
mkeskin17 <57233244+mkeskin17@users.noreply.github.com>
2022-05-26 21:33:26 +0300

    survey screen change + icon addition

--- TermProject-Comp491/CompTermProject/CompTermProject/ViewControllers/SurveyViewController.swift
+++ TermProject-Comp491/CompTermProject/CompTermProject/ViewControllers/SurveyViewController.swift
@@ -16,6 +16,9 @@ class surveyViewController: UIViewController {
     @IBOutlet weak var lastNameField: UITextField!
     @IBOutlet weak var lastName: UILabel!
     @IBOutlet weak var confirmButton: UIButton!
+    @IBOutlet weak var drySkinCheckBox: UIButton!
+    @IBOutlet weak var skinRashCheckBox: UIButton!
+    @IBOutlet weak var hormonalTherapyCheckBox: UIButton!
     
     @IBOutlet weak var genderOption: UITextField!
     @IBOutlet weak var ageOption: UITextField!
@@ -33,16 +36,83 @@ class surveyViewController: UIViewController {
     @objc func handleTap() {
         nameField.resignFirstResponder() // dismiss keyoard
         lastNameField.resignFirstResponder()
-        lastName.resignFirstResponder()
+        //lastName.resignFirstResponder()
         genderOption.resignFirstResponder()
         ageOption.resignFirstResponder()
-        DrySkin.resignFirstResponder()
-        SkinRash.resignFirstResponder()
-        Hormonal_Therapy.resignFirstResponder()
+        //DrySkin.resignFirstResponder()
+        //SkinRash.resignFirstResponder()
+        //Hormonal_Therapy.resignFirstResponder()
+    }
+    
+    let checkedImage = UIImage(named: "checked")! as UIImage
+    let uncheckedImage = UIImage(named: "unchecked")! as UIImage
+    
+    var isDrySkinChecked: Bool = false
+    var isSkinRashChecked: Bool = false
+    var isHormonalTherapyChecked: Bool = false
+    
+    @IBAction  func buttonClicked(sender: UIButton) {
+        print(isDrySkinChecked)
+        if(isDrySkinChecked == false) {
+            print("checked now")
+            isDrySkinChecked = true
+            drySkinCheckBox.setImage(checkedImage, for: UIControl.State.normal)
+        } else {
+            print("unchecked now")
+            isDrySkinChecked = false
+            drySkinCheckBox.setImage(uncheckedImage, for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func skinRashButtonClicked(sender: UIButton) {
+        print(isSkinRashChecked)
+        if(isSkinRashChecked == false) {
+            print("checked now skin rash")
+            isSkinRashChecked = true
+            skinRashCheckBox.setImage(checkedImage, for: UIControl.State.normal)
+        } else {
+            print("unchecked now skin rash")
+            isSkinRashChecked = false
+            skinRashCheckBox.setImage(uncheckedImage, for: UIControl.State.normal)
+        }
+    }
+    
+    @IBAction func hormonalTherapyButtonClicked(sender: UIButton) {
+        print(isHormonalTherapyChecked)
+        if(isHormonalTherapyChecked == false) {
+            print("checked now hormon")
+            isHormonalTherapyChecked = true
+            hormonalTherapyCheckBox.setImage(checkedImage, for: UIControl.State.normal)
+        } else {
+            print("unchecked now hormon")
+            isHormonalTherapyChecked = false
+            hormonalTherapyCheckBox.setImage(uncheckedImage, for: UIControl.State.normal)
+        }
     }
     
     @IBAction func confirmPressed(_ sender: Any) {
-        guard let name = nameField.text,let lastName = lastNameField.text,let gender = genderOption.text,let age = ageOption.text,let DrySkin = DrySkin.text,let SkinRash = SkinRash.text,let hormon = Hormonal_Therapy.text else{
+        var dryskin: Optional<Any>
+        if(isDrySkinChecked == false) {
+            dryskin = "No"
+        } else {
+            dryskin = "Yes"
+        }
+        
+        var skinrash: Optional<Any>
+        if(isSkinRashChecked == false) {
+            skinrash = "No"
+        } else {
+            skinrash = "Yes"
+        }
+        
+        var hormonaltherapy: Optional<Any>
+        if(isHormonalTherapyChecked == false) {
+            hormonaltherapy = "No"
+        } else {
+            hormonaltherapy = "Yes"
+        }
+        
+        guard let name = nameField.text,let lastName = lastNameField.text,let gender = genderOption.text,let age = ageOption.text,let DrySkin = dryskin,let SkinRash = skinrash,let hormon = hormonaltherapy else{
             return
         }
         guard let userID = Auth.auth().currentUser?.uid else{
